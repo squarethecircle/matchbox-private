fbc8ec4291df60fcea02fd27eba33b05540e31b5
squarethecircle <sachith96@gmail.com>
2015-03-02 02:12:46 -0500

    fixed model indexing

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app import db
 
 class Match(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
-	friend1 = db.Column(db.String(64))
-	friend2 = db.Column(db.String(64))
-	matcher = db.Column(db.String(64))
+	friend1 = db.Column(db.String(64), index=True)
+	friend2 = db.Column(db.String(64), index=True)
+	matcher = db.Column(db.String(64), index=True)
 
