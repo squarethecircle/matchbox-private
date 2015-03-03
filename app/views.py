dd44db3bd7b08815c4af9c14f959d2db73f6cc38
squarethecircle <sachith96@gmail.com>
2015-03-02 20:55:17 -0500

    fixed KeyError

--- app/views.py
+++ app/views.py
@@ -28,7 +28,7 @@ def index():
 @app.route('/match')
 def match():
 	#resp = facebook.get('me/friends?fields=name,id,education')
-	if session.get('friends') = None:
+	if session.get('friends') is None:
 		session['friends'] = facebook.get('fql?q=SELECT%20name%2Cuid%2Cpic_big%2Crelationship_status%2Csex%20FROM%20user%20WHERE%20uid%20IN%20(SELECT%20uid1%20FROM%20friend%20WHERE%20uid2%3Dme())%20and%20(%27Yale%20University%27%20in%20education%20or%20%27Yale%27%20in%20affiliations)')
 	return jsonify(session['friends'])
 
