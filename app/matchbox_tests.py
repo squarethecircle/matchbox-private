b436ef278ca7e2f1e185b63e4a958e4ac428744c
Devansh <devanshtandon@gmail.com>
2015-04-01 03:10:04 -0400

    build session

--- app/matchbox_tests.py
+++ app/matchbox_tests.py
@@ -7,8 +7,42 @@ lifestyle_male_friends = []
 lifestyle_female_friends = []
 lifestyle_ids = [100000117930891, 1673808394, 749512978, 629263828, 1120293045, 100000279378280, 1646941022, 644659874, 707859779, 774168034, 821596896, 1235948517, 1306399238, 1293191998, 1391794445, 1471153226, 1522524524, 1666913902, 1577529446, 1598222289, 100004191697613, 100000359149448, 100000892201552, 100001288758840]
 top_matches_ids = [(1375642201, 1646941022), (705579939, 100003888319326), (644659874, 1425476801), (707859779, 1321417892), (707859779, 100005920514441), (644659874, 100005920514441), (1375642201, 1522524524), (1306399238, 100000742350322), (1293191998, 100000742350322), (1235948517, 100004191697613), (100000279378280, 1397434942),  (100000486251970, 1490615349),  (100004797271381, 100000163821701), (100000117930891, 1391794445), (100000117930891, 1471153226), (821596896, 1471153226), (774168034,  1397434942), (100000117930891, 1321417892), (100000279378280, 1490615349), (821596896, 1391794445), (774168034, 100001663293430), (100000279378280, 1321417892), (774168034, 100000742350322), (1293191998, 1321417892), (1293191998, 1471153226), (100000279378280, 100000163821701), (1293191998, 1391794445), (100004797271381, 1425476801), (100000892201552, 1425476801), (100000892201552, 1522524524), (100000279378280, 1248783721), (100002804284636, 1248783721)]
-male_friends = [1, 2, 3]
-female_friends = [7, 8, 9]
+male_friends = [{
+      "name": "Mr Test 1", 
+      "relationship_status": "", 
+      "sex": "male", 
+      "uid": 6514
+    }, 
+    {
+      "name": "Mr Test 2", 
+      "relationship_status": "In a relationship", 
+      "sex": "male", 
+      "uid": 502602860
+    }, 
+    {
+      "name": "Mr Test 3", 
+      "relationship_status": null, 
+      "sex": "male", 
+      "uid": 503939168
+    }]
+female_friends = [{
+      "name": "Mrs Test 1", 
+      "relationship_status": "", 
+      "sex": "female", 
+      "uid": 6514
+    }, 
+    {
+      "name": "Mrs Test 2", 
+      "relationship_status": "In a relationship", 
+      "sex": "female", 
+      "uid": 502602860
+    }, 
+    {
+      "name": "Mrs Test 3", 
+      "relationship_status": null, 
+      "sex": "female", 
+      "uid": 503939168
+    }]
 top_matches = []
 most_upvoted_matches = []
 most_voted_matches = []
@@ -39,7 +73,7 @@ class MatchboxTestCase(unittest.TestCase):
         test_match = models.Match(friends=['100100', '100100'], friend_names=['MrTester', 'MrsTester'], matchers=['10010001000'], num_matchers=1, matcher_names=['MrMatcher'], nonmatchers=[], num_nonmatchers=0, nonmatcher_names=[], confirmed=False)
         add_database = test_match.save()
 
-        get_database = models.Match.objects(friends__all=['test1', 'test2']).first()
+        get_database = models.Match.objects(friends__all=['100100', '100100']).first()
         assert(add_database.friends == get_database.friends)
 
     def test_add_match(self):
