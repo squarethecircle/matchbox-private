bad435087ec93466589a6ba59645069e312056ec
Devansh <devanshtandon@gmail.com>
2015-04-01 02:13:29 -0400

    test accept match function

--- app/matchbox_tests.py
+++ app/matchbox_tests.py
@@ -6,29 +6,47 @@ import views
 class MatchboxTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
         app.config['TESTING'] = True
         self.app = app.test_client()
-        init_db()
+        with self.app as c:
+            with c.session_transaction as sess:
+                sess['fbid'] = '12345'
 
     def tearDown(self):
         os.close(self.db_fd)
         os.unlink(app.config['DATABASE'])
 
     def test_database(self):
-        test_match = Match(friends=['test1', 'test2'], friend_names=['MrTester', 'MrsTester'], matchers=[10010001000], num_matchers=1, matcher_names=['MrMatcher'], nonmatchers=[], num_nonmatchers=0, nonmatcher_names=[], confirmed=false)
+        test_match = Match(friends=['100100', '100100'], friend_names=['MrTester', 'MrsTester'], matchers=['10010001000'], num_matchers=1, matcher_names=['MrMatcher'], nonmatchers=[], num_nonmatchers=0, nonmatcher_names=[], confirmed=false)
         add_database = test_match.save()
 
         get_database = Match.objects(friends__all=['test1', 'test2']).first()
         assert(add_database == get_database)
 
-	def test_session(self):
-		assert(session['male_friends'])
-        assert(session['female_friends'])
+    def test_add_match(self):
+        app.post('match', {friend1:'100100', friend2:'100100', 
+            friend1name:'MrTester', friend2name:'MrsTester', result:'accept'})
 
-    def test_facebook_query(self):
-        assert(session.get('facebook_token'))
-        session.get('me')
+        get_database = Match.objects(friends__all=['100100','100100']).first()
+        assert(get_database)
+
+    def test_increment_match(self):
+        test_match = Match(friends=['100100', '100100'], friend_names=['MrTester', 'MrsTester'], matchers=['10010001000'], num_matchers=1, matcher_names=['MrMatcher'], nonmatchers=[], num_nonmatchers=0, nonmatcher_names=[], confirmed=false)
+        add_database = test_match.save()
+
+        app.post('match', {friend1:'100100', friend2:'100100', 
+            friend1name:'MrTester', friend2name:'MrsTester', result:'accept'})
+
+        get_database = Match.object(friends__all=['100100', '100100']).first()
+        assert(get_database.num_matchers==2)
+
+    # def test_session(self):
+    #     assert(session['male_friends'])
+    #     assert(session['female_friends'])
+
+    # def test_facebook_query(self):
+    #     assert(session.get('facebook_token'))
+    #     session.get('me')
 
 if __name__ == '__main__':
     unittest.main()
