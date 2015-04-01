c4d5a29996aee9e122677cbc8c58ebe3c5d2b735
Devansh <devanshtandon@gmail.com>
2015-04-01 01:46:07 -0400

    database test

--- app/matchbox_tests.py
+++ app/matchbox_tests.py
@@ -3,7 +3,7 @@ import unittest
 import tempfile
 import views
 
-class FlaskrTestCase(unittest.TestCase):
+class MatchboxTestCase(unittest.TestCase):
 
     def setUp(self):
         self.db_fd, app.config['DATABASE'] = tempfile.mkstemp()
@@ -15,12 +15,20 @@ class FlaskrTestCase(unittest.TestCase):
         os.close(self.db_fd)
         os.unlink(app.config['DATABASE'])
 
-	def test_login_logout(self):
-		rv = self.app.get('/login', follow_redirects=True)
-		assert(rv)
+    def test_database(self):
+        test_match = Match(friends=['test1', 'test2'], friend_names=['MrTester', 'MrsTester'], matchers=[10010001000], num_matchers=1, matcher_names=['MrMatcher'], nonmatchers=[], num_nonmatchers=0, nonmatcher_names=[], confirmed=false)
+        add_database = test_match.save()
 
-	def test_session_friends(self):
-		assert(session['friends'] != 0)
+        get_database = Match.objects(friends__all=['test1', 'test2']).first()
+        assert(add_database == get_database)
+
+	def test_session(self):
+		assert(session['male_friends'])
+        assert(session['female_friends'])
+
+    def test_facebook_query(self):
+        assert(session.get('facebook_token'))
+        session.get('me')
 
 if __name__ == '__main__':
     unittest.main()
