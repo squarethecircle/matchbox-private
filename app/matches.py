677ca03e3ae8840ab5874b93a257d5b355c81562
William Xu <william.xu@yale.edu>
2015-04-12 17:03:07 -0400

    generalized making matches for non-lifestyle

--- app/matches.py
+++ app/matches.py
@@ -18,41 +18,15 @@ def match():
 			'%27%20in%20affiliations)').data['data']
 	male_friends = []
 	female_friends = []
-	top_matches = []
-# Our initial test group consists of 20-30 friends in Berkeley College. In order to model the same experience our future users will have, 
-# without any preexisting data, we have made a list of all of their facebook ids, stored under lifestyle ids, which serves as a list of their close friends. 
-# This list is incorporated into our algorithm for showing matches.
-	lifestyle_male_friends = []
-	lifestyle_female_friends = []
-	lifestyle_ids = [100000117930891, 1673808394, 749512978, 629263828, 1120293045, 100000279378280, 1646941022, 644659874, 707859779, 
-		774168034, 821596896, 1235948517, 1306399238, 1293191998, 1391794445, 1471153226, 1522524524, 1666913902, 1577529446, 1598222289, 
-		100004191697613, 100000359149448, 100000892201552, 100001288758840]
-	top_matches_ids = [(1375642201, 1646941022), (705579939, 100003888319326), (644659874, 1425476801), (707859779, 1321417892), 
-		(707859779, 100005920514441), (644659874, 100005920514441), (1375642201, 1522524524), (1306399238, 100000742350322), 
-		(1293191998, 100000742350322), (1235948517, 100004191697613), (100000279378280, 1397434942),  (100000486251970, 1490615349),  
-		(100004797271381, 100000163821701), (100000117930891, 1391794445), (100000117930891, 1471153226), (821596896, 1471153226), 
-		(774168034,	1397434942), (100000117930891, 1321417892), (100000279378280, 1490615349), (821596896, 1391794445), 
-		(774168034, 100001663293430), (100000279378280, 1321417892), (774168034, 100000742350322), (1293191998, 1321417892), 
-		(1293191998, 1471153226), (100000279378280, 100000163821701), (1293191998, 1391794445), (100004797271381, 1425476801), 
-		(100000892201552, 1425476801), (100000892201552, 1522524524), (100000279378280, 1248783721), (100002804284636, 1248783721)]
 # Most_upvoted refers to matches with many affirmative responses. Most_voted refers to matches with many responses of any kind.
 	most_upvoted_matches_ids = []
 	most_upvoted_matches = []
-	most_voted_matches_ids = []
-	most_voted_matches = []
 # Filtering out friends in relationships and on our blacklist and sorting them by gender and whether they are a close friend.
 	for friend in session['fixedfriends']:
-		if friend['sex'] == 'male' and friend['relationship_status'] != 'In a relationship':
-			if friend['uid'] in lifestyle_ids:
-				lifestyle_male_friends.append(friend)
-			if friend['uid'] not in blacklist:
-				male_friends.append(friend)
-		elif friend['sex'] == 'female' and friend['relationship_status'] != 'In a relationship':
-			if friend['uid'] in lifestyle_ids:
-				lifestyle_female_friends.append(friend)
-			if friend['uid'] not in blacklist:
-				female_friends.append(friend)
-
+		if friend['sex'] == 'male' and friend['relationship_status'] != 'In a relationship' and friend['uid'] not in blacklist:
+			male_friends.append(friend)
+		elif friend['sex'] == 'female' and friend['relationship_status'] != 'In a relationship' and friend['uid'] not in blacklist:
+			female_friends.append(friend)
 	male_friends_ids = []
 	female_friends_ids = []
 	for friend in male_friends:
@@ -62,60 +36,38 @@ def match():
 # For the matches in our database where the user is friends with both parties, we sort out matches based on their number of upvotes and total votes.
 	for match in Match.objects:
 		if int(match.friends[0]) in male_friends_ids and int(match.friends[1]) in female_friends_ids:
-			if match.num_matchers > 0:
+			if match.num_matchers > 1:
 				most_upvoted_matches_ids.append((int(match.friends[0]),int(match.friends[1])))
-			if match.num_matchers + match.num_nonmatchers > 1:
-				most_voted_matches_ids.append((int(match.friends[0]),int(match.friends[1])))
 # Using the id lists to filter the database matches into the appropriate categories. Also making sure that if the user has seen a match, it is not added.
 	user_obj = User.objects(fbid=session['fbid']).first()
-	for i in range(0, len(top_matches_ids)):
-		for malefriend in male_friends:
-			if malefriend['uid'] == top_matches_ids[i][0]:
-				for femalefriend in female_friends:
-					if femalefriend['uid'] == top_matches_ids[i][1]:
-						if str((malefriend['uid'],femalefriend['uid'])) not in user_obj.seen_top_matches:
-							top_matches.append((malefriend, femalefriend))
-						break
-				break
 	for i in range(0, len(most_upvoted_matches_ids)):
-				if malefriend['uid'] == most_upvoted_matches_ids[i][0]:
-					for femalefriend in female_friends:
-						if femalefriend['uid'] == most_upvoted_matches_ids[i][1]:
-							if str((malefriend['uid'],femalefriend['uid'])) not in user_obj.seen_top_matches:
-								most_upvoted_matches.append((malefriend,femalefriend))
-							break
-					break
-	for i in range(0, len(most_voted_matches_ids)):
-			if malefriend['uid'] == most_voted_matches_ids[i][0]:
+		for malefriend in male_friends:
+			if malefriend['uid'] == most_upvoted_matches_ids[i][0]:
 				for femalefriend in female_friends:
-					if femalefriend['uid'] == most_voted_matches_ids[i][1]:
+					if femalefriend['uid'] == most_upvoted_matches_ids[i][1]:
 						if str((malefriend['uid'],femalefriend['uid'])) not in user_obj.seen_top_matches:
-							most_voted_matches.append((malefriend,femalefriend))
+							most_upvoted_matches.append((malefriend,femalefriend))
 						break
-				break			
+				break		
 # Session is a global dictionary that we use to pass around relevant data between functions.
 	session['male_friends'] = male_friends
 	session['female_friends'] = female_friends
-	session['lifestyle_male_friends'] = lifestyle_male_friends
-	session['lifestyle_female_friends'] = lifestyle_female_friends
-	session['top_matches'] = top_matches
 	session['most_upvoted_matches'] = most_upvoted_matches
-	session['most_voted_matches'] = most_voted_matches
 # We cache one match so that users never have to wait for a match to load. 
 	match_pair = getWeightedMatch(user_obj)
 	match_pair_cache = getWeightedMatch(user_obj)
 
-	acceptpercentfloat = getPercent(match_pair)
-	if acceptpercentfloat == None:
-		acceptpercent = "No Data"
-		rejectpercent = "No Data"
+	acceptpercentfloatcache = getPercent(match_pair_cache)
+	if acceptpercentfloatcache == None:
+		acceptpercentcache = "No Data"
+		rejectpercentcache = "No Data"
 	else:
-		acceptpercent = str(acceptpercentfloat) + "%"
-		rejectpercent = str(100-acceptpercentfloat) + "%"
+		acceptpercentcache = str(acceptpercentfloatcache) + "%"
+		rejectpercentcache = str(100-acceptpercentfloatcache) + "%"
 
 	new_match={'boy':match_pair_cache[0]['name'],'girl':match_pair_cache[1]['name'],'boypp':getPhoto(match_pair_cache[0]['uid']),
 				'girlpp':getPhoto(match_pair_cache[1]['uid']),'boyid':match_pair_cache[0]['uid'],
-				'girlid':match_pair_cache[1]['uid'],'acceptpercent':acceptpercent,'rejectpercent':rejectpercent}
+				'girlid':match_pair_cache[1]['uid'],'acceptpercent':acceptpercentcache,'rejectpercent':rejectpercentcache}
 	return render_template('match.html',boy=match_pair[0]['name'], girl=match_pair[1]['name'],
 			boypp=getPhoto(match_pair[0]['uid']),girlpp=getPhoto(match_pair[1]['uid']),
 			boyid=match_pair[0]['uid'],girlid=match_pair[1]['uid'],acceptpercent="",rejectpercent="",
@@ -171,6 +123,15 @@ def acceptMatch():
 				'girlid':match_pair[1]['uid'],'acceptpercent':acceptpercent,'rejectpercent':rejectpercent}
 	return jsonify(new_match)
 
+@app.route('/jsonifyMatch')
+def jsonifyMatch():
+	user_obj = User.objects(fbid=session['fbid']).first()
+	match_pair = getWeightedMatch(user_obj)
+	new_match={'boy':match_pair[0]['name'],'girl':match_pair[1]['name'],'boypp':getPhoto(match_pair[0]['uid']),
+				'girlpp':getPhoto(match_pair[1]['uid']),'boyid':match_pair[0]['uid'],
+				'girlid':match_pair[1]['uid']}	
+	return jsonify(new_match)
+
 # A function to query an appropriately sized and croped profile picture from facebook, that takes the user fbid as a parameter.
 def getPhoto(uid):
 	if app.config['TESTING'] == True:
@@ -178,32 +139,12 @@ def getPhoto(uid):
 	photo = facebook.get('fql?q=SELECT%20pic_crop%20from%20profile%20where%20id%3D'+str(uid)).data['data'][0]['pic_crop']
 	return photo['uri']
 
-# A function that uses weighted percentages to return a semi-random match to display to our user. In its current form, we are displaying 
-# 1/8 "top matches", 1/8 close friend matches, 1/8 most upvoted matches, 1/8 most voted on matches, and 1/2 completely random matches.
+# A function that uses weighted percentages to return a semi-random match to display to our user.
 def getWeightedMatch(user_obj):
-	x = randint(1,16)
-	if (x <= 2) and session['top_matches']:
-		match_pair = (session['top_matches'][randint(0,len(session['top_matches'])-1)])
-		session['top_matches'].remove(match_pair)
-		user_obj.seen_top_matches.append(str((match_pair[0]['uid'],match_pair[1]['uid'])))
-		user_obj.save()
-	elif x == 3 and session['lifestyle_male_friends']:
-		match_pair = (session['lifestyle_male_friends'][randint(0,len(session['lifestyle_male_friends'])-1)],
-			session['female_friends'][randint(0,len(session['female_friends'])-1)])
-	elif x == 4 and session['lifestyle_female_friends']:
-		match_pair = (session['male_friends'][randint(0,len(session['male_friends'])-1)],
-			session['lifestyle_female_friends'][randint(0,len(session['lifestyle_female_friends'])-1)])
-	elif x >= 5 and x <= 6 and session['most_upvoted_matches']:
+	x = randint(1,4)
+	if x == 1 and session['most_upvoted_matches']:
 		match_pair = (session['most_upvoted_matches'][randint(0,len(session['most_upvoted_matches'])-1)])
 		session['most_upvoted_matches'].remove(match_pair)
-		if match_pair in session['most_voted_matches']:
-			session['most_voted_matches'].remove(match_pair)
-		user_obj.seen_top_matches.append(str((match_pair[0]['uid'],match_pair[1]['uid'])))
-		user_obj.save()
-	elif x >= 7 and x <= 8 and session['most_voted_matches']:
-		match_pair = (session['most_voted_matches'][randint(0,len(session['most_voted_matches'])-1)])
-		if match_pair in session['most_upvoted_matches']:
-			session['most_upvoted_matches'].remove(match_pair)
 		user_obj.seen_top_matches.append(str((match_pair[0]['uid'],match_pair[1]['uid'])))
 		user_obj.save()
 	else:
